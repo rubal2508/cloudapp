bce596da91ff50a67694c6a5c0782ba69d64e179
Aman Garg <42492732+rubal2508@users.noreply.github.com>
2024-05-27 19:59:28 +0530

    Revert "tekion"
    This reverts commit 508c8d14e047ab522f5c5d563dc72b26e91a26ff.

--- src/main/java/org/amangarg/cloudapp/repo/PartyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.amangarg.cloudapp.repo;
-
-import org.amangarg.cloudapp.model.Party;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PartyRepository extends JpaRepository<Party, Integer> {
-}
